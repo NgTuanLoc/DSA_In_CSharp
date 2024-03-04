79e11f4d2c99ed27bb74f33c47f9611950738515
Nguyễn Tuấn Lộc <74365102+NgTuanLoc@users.noreply.github.com>
2024-03-04 09:21:06 +0700

    Backtracking - the generation

---
+++ LeetCode/Backtracking/Example.cs
@@ -0,0 +1,43 @@
+﻿namespace DSA.LeetCode.Backtracking;
+public class BacktrackingExample
+{
+    public static void Run()
+    {
+        // !Permutation
+        // var result = Permutation_Solution.Permute([1, 2, 3]);
+        // foreach (var list in result)
+        // {
+        //     foreach (var item in list)
+        //     {
+        //         Console.Write($"{item} ");
+        //     }
+        //     Console.WriteLine();
+        // }
+
+        // !Subsets
+        // var result = Subsets_Solution.Subsets([1, 2, 3]);
+        // foreach (var list in result)
+        // {
+        //     Console.Write("[");
+        //     foreach (var item in list)
+        //     {
+        //         Console.Write($" {item} ");
+        //     }
+        //     Console.Write("]");
+        //     Console.WriteLine();
+        // }
+
+        // !Combinations
+        var result = Combination_Solution.Combine(4, 2);
+        foreach (var list in result)
+        {
+            Console.Write("[");
+            foreach (var item in list)
+            {
+                Console.Write($" {item} ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LeetCode/Backtracking/Generation.cs
@@ -0,0 +1,82 @@
+﻿namespace DSA.LeetCode.Backtracking;
+
+
+// !https://leetcode.com/problems/permutations/description/
+public class Permutation_Solution
+{
+    public static IList<IList<int>> Permute(int[] nums)
+    {
+        IList<IList<int>> result = [];
+        Backtracking([], result, nums);
+        return result;
+    }
+
+    private static void Backtracking(List<int> curr, IList<IList<int>> result, int[] nums)
+    {
+        if (curr.Count == nums.Length)
+        {
+            result.Add([.. curr]);
+            return;
+        }
+
+        foreach (var item in nums)
+        {
+            if (!curr.Contains(item))
+            {
+                curr.Add(item);
+                Backtracking(curr, result, nums);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
+    }
+}
+
+class Subsets_Solution
+{
+
+    public static IList<IList<int>> Subsets(int[] nums)
+    {
+        // https://leetcode.com/problems/subsets/
+        IList<IList<int>> result = [];
+        Backtracking([], result, nums, 0);
+        return result;
+    }
+
+    private static void Backtracking(List<int> curr, IList<IList<int>> result, int[] nums, int i)
+    {
+        if (curr.Count > nums.Length) return;
+        result.Add([.. curr]);
+        for (int j = i; j < nums.Length; j++)
+        {
+            curr.Add(nums[j]);
+            Backtracking(curr, result, nums, j + 1);
+            curr.RemoveAt(curr.Count - 1);
+        }
+    }
+}
+
+public class Combination_Solution
+{
+    public static IList<IList<int>> Combine(int n, int k)
+    {
+        IList<IList<int>> result = [];
+        Backtracking([], result, n, k, 1);
+        return result;
+    }
+
+    private static void Backtracking(List<int> curr, IList<IList<int>> result, int n, int k, int i)
+    {
+        if (curr.Count == k)
+        {
+            result.Add([.. curr]);
+            return;
+        }
+
+        for (int j = i; j <= n; j++)
+        {
+            curr.Add(j);
+            Backtracking(curr, result, n, k, j + 1);
+            curr.RemoveAt(curr.Count - 1);
+        }
+    }
+}
