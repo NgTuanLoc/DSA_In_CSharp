3557ff5da2e801dde30968ede03126086d68001f
NgTuanLoc <tuanloc2352000@gmail.com>
2026-05-16 22:02:11 +0700

    feat: add DoublyLinkedList Push and Get with bidirectional traversal

--- src/DSA.Tests/DataStructure/DoublyLinkedListTests.cs
+++ src/DSA.Tests/DataStructure/DoublyLinkedListTests.cs
@@ -11,4 +11,40 @@ public class DoublyLinkedListTests
 
         Assert.Equal(0, list.GetLength());
     }
+
+    [Fact]
+    public void Push_OnEmptyList_SetsHeadAndTailToSameNode()
+    {
+        var list = new DoublyLinkedList();
+
+        list.Push(10);
+
+        Assert.Equal(1, list.GetLength());
+        Assert.Equal(10, list.Get(0));
+    }
+
+    [Fact]
+    public void Push_MultipleValues_LinksTailToTail()
+    {
+        var list = new DoublyLinkedList();
+
+        list.Push(1);
+        list.Push(2);
+        list.Push(3);
+
+        Assert.Equal(3, list.GetLength());
+        Assert.Equal(1, list.Get(0));
+        Assert.Equal(2, list.Get(1));
+        Assert.Equal(3, list.Get(2));
+    }
+
+    [Fact]
+    public void Get_IndexOutOfRange_ReturnsNull()
+    {
+        var list = new DoublyLinkedList();
+        list.Push(1);
+
+        Assert.Null(list.Get(5));
+        Assert.Null(list.Get(-1));
+    }
 }
--- src/DSA/DataStructure/DoublyLinkedList.cs
+++ src/DSA/DataStructure/DoublyLinkedList.cs
@@ -14,4 +14,41 @@ public class DoublyLinkedList
     private int _length;
 
     public int GetLength() => _length;
+
+    public void Push(int value)
+    {
+        var node = new DoublyNode(value);
+        if (_tail == null)
+        {
+            _head = node;
+            _tail = node;
+        }
+        else
+        {
+            node.Prev = _tail;
+            _tail.Next = node;
+            _tail = node;
+        }
+        _length++;
+    }
+
+    public int? Get(int index)
+    {
+        if (index < 0 || index >= _length) return null;
+
+        DoublyNode? current;
+        if (index < _length / 2)
+        {
+            current = _head;
+            for (var i = 0; i < index; i++)
+                current = current!.Next;
+        }
+        else
+        {
+            current = _tail;
+            for (var i = _length - 1; i > index; i--)
+                current = current!.Prev;
+        }
+        return current!.Value;
+    }
 }
