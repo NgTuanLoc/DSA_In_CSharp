34e6be71bb6bd7d9c688625c3faf15aaf82a5708
NgTuanLoc <tuanloc2352000@gmail.com>
2026-05-16 22:04:26 +0700

    feat: add MinHeap.Insert and Peek with bubble-up

--- src/DSA.Tests/DataStructure/MinHeapTests.cs
+++ src/DSA.Tests/DataStructure/MinHeapTests.cs
@@ -30,4 +30,53 @@ public class MinHeapTests
         Assert.Equal(0, heap.Count);
         Assert.True(heap.IsEmpty);
     }
+
+    [Fact]
+    public void Peek_OnEmptyHeap_ThrowsInvalidOperationException()
+    {
+        var heap = new MinHeap();
+
+        Assert.Throws<InvalidOperationException>(() => heap.Peek());
+    }
+
+    [Fact]
+    public void Insert_SingleValue_PeeksThatValue()
+    {
+        var heap = new MinHeap();
+
+        heap.Insert(42);
+
+        Assert.Equal(1, heap.Count);
+        Assert.Equal(42, heap.Peek());
+    }
+
+    [Fact]
+    public void Insert_MultipleValues_PeekReturnsSmallest()
+    {
+        var heap = new MinHeap();
+
+        heap.Insert(5);
+        heap.Insert(3);
+        heap.Insert(8);
+        heap.Insert(1);
+        heap.Insert(4);
+
+        Assert.Equal(5, heap.Count);
+        Assert.Equal(1, heap.Peek());
+    }
+
+    [Fact]
+    public void Insert_DescendingValues_PeekTracksMin()
+    {
+        var heap = new MinHeap();
+
+        heap.Insert(10);
+        Assert.Equal(10, heap.Peek());
+        heap.Insert(5);
+        Assert.Equal(5, heap.Peek());
+        heap.Insert(2);
+        Assert.Equal(2, heap.Peek());
+        heap.Insert(1);
+        Assert.Equal(1, heap.Peek());
+    }
 }
--- src/DSA/DataStructure/MinHeap.cs
+++ src/DSA/DataStructure/MinHeap.cs
@@ -8,4 +8,34 @@ public class MinHeap
     public bool IsEmpty => _data.Count == 0;
 
     public void Clear() => _data.Clear();
+
+    public int Peek()
+    {
+        if (IsEmpty)
+            throw new InvalidOperationException("Heap is empty");
+
+        return _data[0];
+    }
+
+    public void Insert(int value)
+    {
+        _data.Add(value);
+        BubbleUp();
+    }
+
+    private void BubbleUp()
+    {
+        var index = _data.Count - 1;
+        var inserted = _data[index];
+        while (index > 0)
+        {
+            var parentIndex = (index - 1) / 2;
+            var parent = _data[parentIndex];
+            if (parent <= inserted) break;
+
+            _data[parentIndex] = inserted;
+            _data[index] = parent;
+            index = parentIndex;
+        }
+    }
 }
